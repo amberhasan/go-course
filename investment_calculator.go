911bacb5f90368ce322d162d07578cb64b822d9a
Amber Hasan <amber.hasan@gmail.com>
2024-07-18 17:07:45 -0500

    Format

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,13 +8,23 @@ import (
 func main() {
 	const inflationRate = 2.5
 	var investmentAmount float64 //default value of 0.0
-	expectedReturnRate := 5.5
-	years := 10.0
+	var years float64
+	var expectedReturnRate float64 = 5.5
 
+	fmt.Println("Enter the investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years) 
+	fmt.Println("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Println("Enter the number of years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
-	fmt.Println("Future value is ", futureValue)
-	fmt.Println("Future real value is ", futureRealValue)
+
+	formattedFV := fmt.Sprintf("Future value is %.2f", futureValue)
+	formattedFRV := fmt.Sprintf("Future real value is %.2f", futureRealValue)
+
+	fmt.Print(formattedFV, formattedFRV)
 }
